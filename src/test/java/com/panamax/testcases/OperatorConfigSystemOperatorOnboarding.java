89f1dd3f889b0b01ad83463776582c88301d8b35
badal16 <badalngandhi@gmail.com>
2020-02-17 13:40:32 +0530

    updated script level changes

--- src/test/java/com/panamax/testcases/OperatorConfigSystemOperatorOnboarding.java
+++ src/test/java/com/panamax/testcases/OperatorConfigSystemOperatorOnboarding.java
@@ -9,7 +9,7 @@ import com.panamax.base.OperatorConfigSystemOperatorOnboardingWeb;
 import com.panamax.init.Common;
 import com.panamax.init.TestDataImport;
 
-public class OperatorConfigSystemOperatorOnboarding extends Common{
+public class OperatorConfigSystemOperatorOnboarding extends Common {
 
 	HomeWeb homePage;
 	OperatorConfigSystemOperatorOnboardingWeb operatorConfigSystemOperatorOnboardingWeb;
@@ -17,18 +17,29 @@ public class OperatorConfigSystemOperatorOnboarding extends Common{
 	int sortCounter = 0;
 
 	/**
-	 * @author shivani.patel 
-	 * For Platform Configuration - SystemOperatorOnboarding - Add
+	 * @author shivani.patel For Platform Configuration - SystemOperatorOnboarding -
+	 *         Add
 	 * @creation date 24/09/2019
 	 */
 	@Test(dataProvider = "SystemOperatorOnboarding_Add", dataProviderClass = TestDataImport.class, description = "Id: AddSystemOperatorOnboarding, Author: shivani.patel")
 	public void addSystemOperatorOnboarding(Map<Object, Object> map) {
-		if (count == 0) {
-			homePage = goToHome();
-			operatorConfigSystemOperatorOnboardingWeb = homePage.clickOnOperatorConfigSystemOperatorOnboarding();
-			count++;
+		try {
+			map.put("Method Name", "addSystemOperatorEntity");
+			if (count == 0) {
+				homePage = goToHome();
+				operatorConfigSystemOperatorOnboardingWeb = homePage.clickOnOperatorConfigSystemOperatorOnboarding();
+				count++;
+			}
+			operatorConfigSystemOperatorOnboardingWeb.addSystemOperatorOnboarding(map, getMapKeys(map));
+			verifyTrue(operatorConfigSystemOperatorOnboardingWeb.verifyAddedSystemOperatorOnboarding(map,
+					getMapKeys(map)));
+
+			map.put("value", 100);
+		} catch (Exception e) {
+			map.put("value", 0);
+			logException(e, map);
+		} finally {
+			logData(map);
 		}
-		operatorConfigSystemOperatorOnboardingWeb.addSystemOperatorOnboarding(map, getMapKeys(map));
-		verifyTrue(operatorConfigSystemOperatorOnboardingWeb.verifyAddedSystemOperatorOnboarding(map, getMapKeys(map)));
 	}
 }
