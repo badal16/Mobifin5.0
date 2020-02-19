ecdcf853dae0a5918160306347442ba7523cef78
dishant-doshi <darshandoshi18@gmail.com>
2020-02-19 17:14:01 +0530

    First Commit
    First Commit

--- src/test/java/com/panamax/base/LoginWeb.java
+++ src/test/java/com/panamax/base/LoginWeb.java
@@ -1,63 +1,56 @@
 package com.panamax.base;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-
-
-public class LoginWeb extends BaseWeb{
+public class LoginWeb extends BaseWeb {
 
 	public LoginWeb(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+
 	By unmTextBox = By.id("inputLoginUsername");
 	By pwdTextBox = By.id("inputLoginPd");
 	By loginBtn = By.id("btnLoginLogin");
-	By languageDropDown = By.id("Language"); 
+	By languageDropDown = By.id("Language");
 
 	/**
 	 * @author vivek.mishra
-	 * @param unm to be send
+	 * @param unm
+	 *            to be send
 	 * @creation date 27/09/2018
 	 */
-	public void sendTextInUnmField(String unm)
-	{
+	public void sendTextInUnmField(String unm) {
 		sendTextInTextBox(unmTextBox, unm);
 	}
-	
+
 	/**
 	 * @author vivek.mishra
-	 * @param pwd to be send
+	 * @param pwd
+	 *            to be send
 	 * @creation date 27/09/2018
 	 */
-	public void sendTextInPwdField(String pwd)
-	{
+	public void sendTextInPwdField(String pwd) {
 		sendTextInTextBox(pwdTextBox, pwd);
 	}
-	
+
 	/**
-	 * @author vivek.mishra
-	 * To click on an element
+	 * @author vivek.mishra To click on an element
 	 * @creation date 27/09/2018
 	 */
-	public void clickOnLoginBtn()
-	{
+	public void clickOnLoginBtn() {
 		clickOnElement(loginBtn);
 	}
-	
-	public void selectLanguage(String language){
+
+	public void selectLanguage(String language) {
 		selectFromDropdown(languageDropDown, language);
 	}
-	
+
 	/**
-	 * @author vivek.mishra
-	 * To login
+	 * @author vivek.mishra To login
 	 * @creation date 27/09/2018
 	 */
-	public void login(String userName, String password)
-	{
+	public void login(String userName, String password) {
 		sendTextInUnmField(userName);
 		sendTextInPwdField(password);
 		clickOnLoginBtn();
